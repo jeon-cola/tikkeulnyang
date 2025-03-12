40c51c4352581eaee505f0f5d54c635880c938d6
hongs <hghg990124@naver.com>
2025-03-12 17:53:26 +0900

    feat: 바텀바 틀 구성

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,9 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <button>확인</button>
-      <button className="longButton">확인</button>
-      <button className="subScribe">rbehr</button>
+      <NavBar/>
     </>
   );
 }
