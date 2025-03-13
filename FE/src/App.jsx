251d451dbe3cc32b352fecbb507a0e8e99602049
김성준 <sjkim2243@gmail.com>
2025-03-13 15:55:55 +0900

    Feat: 더보기 버튼 CSS 구현 완료

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-import Router from './routes'
+import "./App.css";
+import Router from "./routes";
 function App() {
   return (
     <>
-      <Router/>
+      <Router />
     </>
   );
 }
