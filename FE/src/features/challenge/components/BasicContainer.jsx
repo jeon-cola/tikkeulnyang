e41645fb648261ba18dd0176309490638f51151f
김성준 <sjkim2243@gmail.com>
2025-03-14 19:32:34 +0900

    Feat: 카드 박스 컨테이너 구현

--- FE/src/features/challenge/components/BasicContainer.jsx
+++ FE/src/features/challenge/components/BasicContainer.jsx
@@ -8,7 +8,7 @@ import React from "react";
 export default function BasicContainer({ children }) {
   return (
     <>
-      <div className="flex flex-col items-center p-[28px_11px_12px] gap-[22px] relative w-[392px] h-[580px] bg-white rounded-[6px]">
+      <div className="flex flex-col items-center p-[28px_11px_12px] gap-[22px] relative w-[392px] h-auto bg-white rounded-[6px]">
         {/* prop으로 전달받은 내용을 나열 */}
         {children}
       </div>
