b11f366709c474186948627c60946f6272cb31bf
hongseok <hghg990124@naver.com>
2025-03-15 00:02:11 +0900

    Fetch: 카카오 로그인 수정

--- FE/src/features/user/components/Login.jsx
+++ FE/src/features/user/components/Login.jsx
@@ -1,15 +1,11 @@
-import axios from 'axios';
+
 import '/src/index.css'
-import { redirect } from 'react-router-dom';
 
 export default function Login() {
     function KakaoLogin() {
-        console.log(import.meta.env.VITE_APP_KAKAO_API_KEY)
-        window.Kakao.init(import.meta.env.VITE_APP_KAKAO_API_KEY)
-        window.Kakao.Auth.authorize({
-            redirectUri:"http://localhost:5173/user/"
-        })
+        window.location.href = "http://localhost:8080/api/auth/login"
     }
+    
     return(
         <>
             <button className="longButton" onClick={KakaoLogin}>카카오 로그인</button>
