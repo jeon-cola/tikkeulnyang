d90a0cd53b5877735f5d93de8d1b8de7e2b6ab68
hongseok <hghg990124@naver.com>
2025-03-15 21:44:51 +0900

    로그인 및 회원가입 페이지 로직구성

--- FE/src/routes/MyPageRouter.jsx
+++ FE/src/routes/MyPageRouter.jsx
@@ -4,7 +4,7 @@ import MyPageMain from "../features/mypage/MyPageMain";
 export default function MyPageRouter() {
     return(
         <Routes>
-            <Route path="/" element={<MyPageMain/>}/>
+            <Route path="/*" element={<MyPageMain/>}/>
         </Routes>
     )
 }
