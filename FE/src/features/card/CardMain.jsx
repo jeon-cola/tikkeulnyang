f4260f31d1fc75f27507e5393494c99587607508
hongseok <hghg990124@naver.com>
2025-03-16 17:21:43 +0900

    Feat: 카드 추천 component 생성

--- FE/src/features/card/CardMain.jsx
+++ FE/src/features/card/CardMain.jsx
@@ -1,5 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import CardRecommend from "./components/CardRecommend";
+
 export default function CardMain() {
     return (
-        <>CardMain</>
+        <Routes>
+            <Route path="/" element={<CardRecommend/>}/>
+        </Routes>
     )
 }
---
+++ FE/src/features/card/components/CardRecommend.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+
+export default function CardRecommend() {
+  useEffect(() => {
+    
+  })
+  return(
+    <>
+      카드 추천
+    </>
+  )
+}
