646e658b4b8b4b7331a1e54e689dd16200404c55
hongseok <hghg990124@naver.com>
2025-03-17 23:41:50 +0900

    Feat: 버킷리스트 생성

--- FE/dev-dist/sw.js
+++ FE/dev-dist/sw.js
@@ -79,10 +79,10 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
-    "revision": "0.busm5dl752g"
+    "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o5r2rqtr04"
+    "revision": "0.busm5dl752g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
@@ -90,3 +90,4 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
   }));
 
 }));
+
