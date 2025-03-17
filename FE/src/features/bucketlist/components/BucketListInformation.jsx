23267980de02eebaf7bdea41c15d239830cd9c9d
hongs <hghg990124@naver.com>
2025-03-17 13:56:44 +0900

    Feat: 안내문 생성

---
+++ FE/src/features/bucketlist/components/BucketListInformation.jsx
@@ -0,0 +1,39 @@
+export default function BucketListInformation() {
+    return (
+        <div className="flex flex-col justify-center gap-5">
+            <img src="/bucketListImg_1.png" alt="안내문 사진 1" className="w-full scale-[1.2] transform-gpu mb-[30px]"/>
+            <div>
+                <p className="font-semibold text-2xl">목표를 향한</p>
+                <p className="font-semibold text-2xl">나만의 버킷리스트</p>
+            </div>
+            <div>
+                <p>나만의 목표 달성을 위한</p>
+                <p>버킷리스트를 만들어 보세요요</p>
+            </div>
+            <div className="w-full h-[128px] bg-[#FF957A] rounded-[25px] flex items-center justify-between px-7">
+                <div className="text-white text-left">
+                    <p className="font-semibold text-2xl">일본 여행가기</p>
+                    <p className="font-semibold text-2xl">300만원 모으기</p>
+                </div>
+                <img src="/Plane.png" alt="비행기" className="w-25 h-25" />
+            </div>
+            <div className="w-full h-[128px] bg-[#FF957A] rounded-[25px] flex items-center justify-between px-7">
+                <div className="text-white text-left">
+                    <p className="font-semibold text-2xl">자취방 마련하기</p>
+                    <p className="font-semibold text-2xl">500만원 모으기</p>
+                </div>
+                <img src="/Furniture.png" alt="방" className="w-25 h-25" />
+            </div>
+            <div>
+                <p className="font-semibold text-2xl">출석으로</p>
+                <p className="font-semibold text-2xl">목표달성을 한눈에 보기기</p>
+            </div>
+            <div>
+                <p>요일과 매주 저축할 금액을 정해</p>
+                <p>간편하게 송금해 보세요요</p>
+            </div>
+            <img src="/cat_stamp.png" alt="고양이 스탬프" className="w-full scale-[1.2] transform-gpu" />
+            <button className="mb-[10px] mx-auto">생성하기</button>
+        </div>
+    ) 
+}
