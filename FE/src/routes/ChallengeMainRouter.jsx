56726d5a779d12128ee2e08a9f167e677fcc2f07
김성준 <sjkim2243@gmail.com>
2025-03-17 14:59:43 +0900

    Feat: 챌린지 메인페이지 네브바빼고 껍데기 완성

--- FE/src/routes/ChallengeMainRouter.jsx
+++ FE/src/routes/ChallengeMainRouter.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import ChallengeMain from "../features/challenge/ChallengeMain";
 
-export default function ChallengeMainRouter(){
-    return(
-        <Routes>
-            <Route path="/*" element={<ChallengeMain/>}/>
-        </Routes>
-    )
+export default function ChallengeMainRouter() {
+  return (
+    <Routes>
+      <Route path="/*" element={<ChallengeMain />} />
+    </Routes>
+  );
 }
