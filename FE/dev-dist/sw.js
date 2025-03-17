78ecd515f8b3b915eb853d9ff07ac7bbec690f7b
hongs <hghg990124@naver.com>
2025-03-17 17:57:52 +0900

    step2 까지 진행

--- FE/dev-dist/sw.js
+++ FE/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sg50mk5sllo"
+    "revision": "0.28bbdu63qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
