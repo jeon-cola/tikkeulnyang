4b2898d9fed561a8be5db514acbf3792eeab48f5
Seoyeon Kang <ekfa29@naver.com>
2025-03-17 12:00:39 +0900

    Feat: 달력컴포넌트 생성

---
+++ FE/src/features/ledger/components/PersonalLedger.jsx
@@ -0,0 +1,9 @@
+import CustomCalendar from "../../../components/CustomCalendar";
+
+export default function PersonalLedger() {
+  return (
+    <div>
+      <CustomCalendar />
+    </div>
+  );
+}
--- FE/src/features/user/components/Signup.jsx
+++ FE/src/features/user/components/Signup.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import "/src/index.css";
 import { useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import CustomHeader from "../../../components/CustomHeader";
 
 export default function Signup() {
   const [nicknameCheck, setNicknameCheck] = useState(false);
@@ -89,9 +90,10 @@ export default function Signup() {
 
   return (
     <>
-      <header className="w-full h-12 bg-white shadow-md rounded-t-2xl fixed top-0 left-0 right-0 flex-none order-none flex-grow-0 z-10 flex items-center">
+      {/* <header className="w-full h-12 bg-white shadow-md rounded-t-2xl fixed top-0 left-0 right-0 flex-none order-none flex-grow-0 z-10 flex items-center">
         <p className="text-[17px] font-medium ml-6">회원가입</p>
-      </header>
+      </header> */}
+      <CustomHeader title="회원가입" />
 
       <form className="flex flex-col gap-[30px]">
         {/* 닉네임 */}
