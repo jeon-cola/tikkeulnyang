75eaec4c67131caec169b7807f48d83607e97c3f
김성준 <sjkim2243@gmail.com>
2025-03-17 16:27:56 +0900

    Feat: 챌린지 상세 페이지 생성 및 라우팅 추가

--- FE/src/features/challenge/components/ChallengeDesc.jsx
+++ FE/src/features/challenge/components/ChallengeDesc.jsx
@@ -1,7 +1,7 @@
 export default function ChallengeDesc({ type, button }) {
   return (
     <>
-      <div className="flex w-[370px]">
+      <div className="flex w-[370px] ">
         <div className="w-full h-auto flex-none order-0 self-stretch flex-grow-0">
           {/* 추천 챌린지 텍스트 */}
           <h2 className="absolute w-[154.38px] left-0 top-0 font-inter font-semibold text-[23px] leading-7 text-black">
--- FE/src/routes/ChallengeMainRouter.jsx
+++ FE/src/routes/ChallengeMainRouter.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import ChallengeMain from "../features/challenge/ChallengeMain";
+import ChallengeDetail from "@/features/challenge/ChallengeDetail";
 
 export default function ChallengeMainRouter() {
   return (
     <Routes>
-      <Route path="/*" element={<ChallengeMain />} />
+      <Route path="/" element={<ChallengeMain />} />
+      <Route path="/:id" element={<ChallengeDetail />} />
     </Routes>
   );
 }
