4b17fffd44453a1c18b3b482eb0a24cd56c54df3
hongseok <hghg990124@naver.com>
2025-03-17 23:41:50 +0900

    Feat: 버킷리스트 생성

--- FE/src/features/bucketlist/bucketListMain.jsx
+++ FE/src/features/bucketlist/bucketListMain.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import BucketListInformation from "./components/BucketListInformation";
 import BucketListStep1 from "./components/BucketListStep1";
 import BucketListStep2 from "./components/BucketListStep2";
+import BucketListStep3 from "./components/BucketListStep3";
+import List from "./components/List";
 
 export default function BucketListMain() {
     return (
@@ -9,6 +11,8 @@ export default function BucketListMain() {
             <Route path="/" element={<BucketListInformation/>}/>
             <Route path="step1" element={<BucketListStep1/>}/>
             <Route path="step2" element={<BucketListStep2/>}/>
+            <Route path="step3" element={<BucketListStep3/>}/>
+            <Route path="list" element={<List/>}/>
         </Routes>
     )
 }
