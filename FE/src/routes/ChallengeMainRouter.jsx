3f68a057c08c6d60c19a6f8bee29a4172b3d13c6
김성준 <sjkim2243@gmail.com>
2025-03-19 08:14:11 +0900

    Feat: 챌린지 입장 페이지 초안 구현 완료

--- FE/src/routes/ChallengeMainRouter.jsx
+++ FE/src/routes/ChallengeMainRouter.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import ChallengeMain from "../features/challenge/ChallengeMain";
 import ChallengeDetail from "@/features/challenge/ChallengeDetail";
+import ChallengeEnter from "@/features/challenge/ChallengeEnter";
 
 export default function ChallengeMainRouter() {
   return (
     <Routes>
       <Route path="/" element={<ChallengeMain />} />
       <Route path="/:id" element={<ChallengeDetail />} />
+      <Route path="/enter/:id" element={<ChallengeEnter />} />
     </Routes>
   );
 }
