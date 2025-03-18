9e84a0c7c19ff00042c1612797bd13a95baa1e86
SSAFY <hghg6829@naver.com>
2025-03-18 17:06:44 +0900

    Fix: 예산 설정 post jwt 수정

--- BE/src/main/java/com/c107/budget/controller/BudgetController.java
+++ BE/src/main/java/com/c107/budget/controller/BudgetController.java
@@ -2,11 +2,17 @@ package com.c107.budget.controller;
 
 import com.c107.budget.dto.BudgetRequestDto;
 import com.c107.budget.dto.BudgetResponseDto;
+import com.c107.budget.entity.BudgetEntity;
 import com.c107.budget.service.BudgetService;
+import com.c107.common.util.ResponseUtil;
+import com.c107.subscribe.entity.SubscribeEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/budget")
@@ -16,17 +22,19 @@ public class BudgetController {
 
     @PostMapping("/plan")
     public ResponseEntity<?> createBudgetPlan(
-            @RequestHeader("Authorization") String token,
-            @RequestBody BudgetRequestDto requestDto
-    ) {
+            @AuthenticationPrincipal String email,
+            @RequestBody BudgetRequestDto requestDto) {
 
-        try {
+        BudgetResponseDto responseDto = budgetService.createBudget(email, requestDto);
+        return ResponseUtil.success("예산 계획이 성공적으로 등록되었습니다.", responseDto);
 
-            BudgetResponseDto responseDto = budgetService.createBudget(token, requestDto);
-            return ResponseEntity.ok(responseDto);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("예산 설정 중 오류 발생: " + e.getMessage());
-        }
+
+//        try {
+//            BudgetResponseDto responseDto = budgetService.createBudget(token, requestDto);
+//            return ResponseEntity.ok(responseDto);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("예산 설정 중 오류 발생: " + e.getMessage());
+//        }
     }
 }
