16d54b8b05f79f09f8fb2d1c17c1e693f5534146
이명성 <goodthinglee@gmail.com>
2025-03-19 16:16:53 +0900

    fix: dev-dist 원상태로 복구

--- FE/dev-dist/sw.js
+++ FE/dev-dist/sw.js
@@ -79,10 +79,10 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "registerSW.js",
-    "revision": "3ca0b8505b4bec776b69afdba2768812"
+    "revision": ""
   }, {
     "url": "index.html",
-    "revision": "0.uv8dfrcugag"
+    "revision": "0.po8rqkatbj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
@@ -90,3 +90,4 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
   }));
 
 }));
+
