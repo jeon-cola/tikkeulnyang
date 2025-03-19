d0569daca014cfa60677844fb20177ecb869e126
Seoyeon Kang <ekfa29@naver.com>
2025-03-19 11:24:16 +0900

    Feat: 가계부 개인 컴포넌트 생성

---
+++ FE/src/components/Container.jsx
@@ -0,0 +1,7 @@
+export default function Container({ children }) {
+  return (
+    <div className="flex flex-col items-start p-[30px_20px_12px] gap-3 absolute w-full min-h-screen left-0 top-[49px] overflow-y-scroll bg-[#F7F7F7]">
+      {children}
+    </div>
+  );
+}
