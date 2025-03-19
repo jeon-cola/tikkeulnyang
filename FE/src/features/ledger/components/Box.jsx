29de4e91458a3e539d134ccf063650d42099783b
kang seoyeon <ekfa29@naver.com>
2025-03-20 07:57:56 +0900

    Design: 개인 가계부 컴포넌트 css 적용

--- FE/src/features/ledger/components/Box.jsx
+++ FE/src/features/ledger/components/Box.jsx
@@ -1,7 +1,15 @@
-export default function Box({ text }) {
+export default function Box({ text, variant, children }) {
+  const baseStyles = "w-full h-[40px] flex items-center justify-between rounded-md shadow-sm pl-[10px] pr-[10px]"
+  const variants = {
+    title: "font-bold text-black bg-white",
+    highlight: "font-medium text-black bg-[#FFF0BA]"
+  }
   return (
-    <div className="w-full h-[40px] bg-white flex items-center justify-left rounded-md shadow-sm pl-[10px]">
-      {text}
-    </div>
-  );
+  <div className={`${baseStyles} ${variants[variant]}`}>
+      <span>{text}</span>
+      <div className="flex justify-end items-center w-auto">{children}</div>
+          
+  </div>
+  )
 }
+
