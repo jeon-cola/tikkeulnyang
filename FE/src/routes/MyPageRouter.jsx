a9bd45e9ea7c821e507be70c8688f5a8e95ba1ae
dydkssk147258 <dydkssk147258@naver.com>
2025-03-21 10:14:25 +0900

    Fix: 프론트 임포트 문 수정

--- FE/src/routes/MyPageRouter.jsx
+++ FE/src/routes/MyPageRouter.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import MyPageMain from "../features/mypage/myPageMain";
+import MyPageMain from "../features/mypage/MyPageMain";
 
 export default function MyPageRouter() {
-    return(
-        <Routes>
-            <Route path="/*" element={<MyPageMain/>}/>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/*" element={<MyPageMain />} />
+    </Routes>
+  );
 }
