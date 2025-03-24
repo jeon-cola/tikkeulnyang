ac69e2a8f91e505fc076da9e8b27254f88c998d8
김성준 <sjkim2243@gmail.com>
2025-03-24 10:26:28 +0900

    Feat: 챌린지 상세조회 json-server 통신 구현

--- FE/src/features/challenge/servies/ChallengeService.js
+++ FE/src/features/challenge/servies/ChallengeService.js
@@ -15,11 +15,26 @@ export const ChallengeService = {
     }
   },
 
+  // 참여이력 전체조회
   getHistory: async (page, size) => {
     try {
       const response = await Api.get(
         //`/challenge/history?page=${page}&size=${size}`
         `/api_challenge_history?page=${page}&size=${size}`
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  },
+
+  // 현재 참여중인 챌린지 상세조회
+  getCurrChallenge: async (challengeId) => {
+    try {
+      const response = await Api.get(
+        //`/api/challenge/current?challenge_id=${challengeId}`
+        `/api_challenge_current?challenge_id=${challengeId}`
       );
       return response;
     } catch (error) {
