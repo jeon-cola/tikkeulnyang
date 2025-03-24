a89d5ae00e29980d076b1ccc5f4fe2463bdb19c5
김성준 <sjkim2243@gmail.com>
2025-03-24 23:36:04 +0900

    Feat: 참여이력에 있는 카드 클릭시 상세 페이지로 넘어가도록 설정

--- FE/src/services/Api.jsx
+++ FE/src/services/Api.jsx
@@ -1,14 +1,11 @@
 import axios from "axios";
 
 const Api = axios.create({
-  //baseURL: "http://localhost:8080/api",
-  baseURL: "http://localhost:3001",
-  withCredentials: false, // json-server는 credentials가 필요없으므로 false로 설정
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "http://localhost:8080/api",
+  withCredentials: true,
 });
 
 export default Api;
+
 // 응답 인터셉터
 Api.interceptors.response.use();
