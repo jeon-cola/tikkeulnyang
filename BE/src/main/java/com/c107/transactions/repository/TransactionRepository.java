a29ab13a81fb681d174069157c3085d097575b59
이명성 <goodthinglee@gmail.com>
2025-03-24 15:04:17 +0900

    feat: Transaction 생성

---
+++ BE/src/main/java/com/c107/transactions/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.c107.transactions.repository;
+
+import com.c107.transactions.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+}
