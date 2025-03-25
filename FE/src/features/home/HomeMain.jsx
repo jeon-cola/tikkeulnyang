ebe77f9da4d8e8039d001f3dca50ae2031f38c38
hongs <hghg990124@naver.com>
2025-03-25 09:58:30 +0900

    Feat: redux 생성

--- FE/src/features/home/HomeMain.jsx
+++ FE/src/features/home/HomeMain.jsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import Api from "../../services/Api"
+import { setEmail, setNickName } from "../user/UserSlice"
+import axios from "axios";
+
 export default function HomeMain() {
+    const dispatch = useDispatch();
+    const {nickName, email} = useSelector(state => state.user)
+    useEffect(()=> {
+        const fetchData = async ()=> {
+            try {
+                const response = await axios.get("http://localhost:8080/api/user/me")
+                if (response.status === "success") {
+                    dispatch(setEmail(response.data.email))
+                    console.log("email",response.data.email)
+                    dispatch(setNickName(response.data.nickname))
+                    console.log("nickname",response.data.nickname)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData();
+    },[])
     return (
         <>
-            <button className="longButton"></button>
+            {email}
+            {nickName}
         </>
     )
 }
