7cacc95a1fae60b90ab3bc14de004bbc794db6e7
hongs <hghg990124@naver.com>
2025-03-25 14:21:27 +0900

    Feat: 로그아웃 연결

--- FE/src/features/home/HomeMain.jsx
+++ FE/src/features/home/HomeMain.jsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import Api from "../../services/Api"
-import { setEmail, setNickName } from "../user/UserSlice"
+import { setEmail, setNickName, setProfileImg } from "../user/UserSlice"
 import axios from "axios";
 
 export default function HomeMain() {
     const dispatch = useDispatch();
-    const {nickName, email} = useSelector(state => state.user)
+    const {nickName, email, profileImg} = useSelector(state => state.user)
     useEffect(()=> {
         const fetchData = async ()=> {
             try {
                 const response = await Api.get("/api/user/me")
+                // const response = await axios.get("http://localhost:8080/api/user/me",{
+                //     withCredentials:true
+                // })
                 console.log(response.data)
                 if (response.data.body.status === "success") {
                     const userData = response.data.body.data
@@ -24,7 +27,7 @@ export default function HomeMain() {
             }
         }
         fetchData();
-    },[])
+    },[dispatch])
     return (
         <>
         <p>
