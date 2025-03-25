6b3de9e4ade81e2a86f065424a839e41305335ea
SSAFY <hghg6829@naver.com>
2025-03-25 15:24:32 +0900

    Fix: 가계부 카테고리 맵핑 및 결제내역 api 수정

--- BE/src/main/java/com/c107/paymenthistory/repository/BudgetCategoryRepository.java
+++ BE/src/main/java/com/c107/paymenthistory/repository/BudgetCategoryRepository.java
@@ -4,6 +4,10 @@ import com.c107.paymenthistory.entity.BudgetCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BudgetCategoryRepository extends JpaRepository<BudgetCategoryEntity, Integer> {
+    Optional<BudgetCategoryEntity> findByCategoryName(String categoryName);
+
 }
