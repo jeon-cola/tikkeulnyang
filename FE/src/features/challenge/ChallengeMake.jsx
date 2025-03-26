ec70b5bcec9c4189b0ed6f5f3b5e240a9c27b320
김성준 <sjkim2243@gmail.com>
2025-03-26 16:13:45 +0900

    챌린지 참여 axios추가 및 테스트중

--- FE/src/features/challenge/ChallengeMake.jsx
+++ FE/src/features/challenge/ChallengeMake.jsx
@@ -2,17 +2,47 @@ import CustomHeader from "@/components/CustomHeader";
 import NavBar from "@/components/NavBar";
 import { useNavigate } from "react-router-dom";
 import CustomCalendar from "@/components/CustomCalendar";
+import { ChallengeService } from "@/features/challenge/servies/ChallengeService";
+import { useState } from "react";
 
 export default function ChallengeMake() {
   const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(`/challenge`); // 챌린지 메인 페이지로 이동
+
+  const [challengeData, setChallengeData] = useState({
+    challengeName: "",
+    description: "",
+    targetAmount: "",
+    startDate: "",
+    endDate: "",
+    limitAmount: "",
+    challengeCategory: "",
+    imageUrl: "",
+    maxParticipants: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setChallengeData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await ChallengeService.postChallengeCreate(challengeData);
+      //TODO : 추후에 챌린지 알림 페이지도 이동하게끔 수정
+      navigate(`/challenge`);
+    } catch (error) {
+      console.error("챌린지 생성 실패", error);
+      alert("챌린지 생성에 실패했습니다. ");
+    }
   };
 
   return (
     <>
       <CustomHeader title="챌린지 생성" />
-      <div className="flex flex-col items-start p-[30px_10px_12px] gap-3 absolute w-full min-h-screen left-0 top-[49px] overflow-y-scroll bg-[#F7F7F7]">
+      <div className="flex flex-col items-start p-[30px_10px_82px] gap-3 absolute w-full min-h-screen left-0 top-[49px] overflow-y-scroll bg-[#F7F7F7]">
         <div className="flex flex-col items-center p-[12px_20px_12px] gap-[22px] relative w-full h-auto bg-white rounded-[6px]">
           <div className="flex flex-col items-start p-0 w-[347px] h-[70px]">
             <div className="w-full h-[32px] order-none flex-none">
@@ -27,7 +57,10 @@ export default function ChallengeMake() {
             {/* 입력창 */}
             <div className="box-border w-full h-[38px] bg-white border border-[#DFDFDF] shadow-[0px_0.5px_2px_rgba(0,0,0,0.25)] rounded-[6px] order-1 flex-none">
               <input
-                type="email"
+                type="text"
+                name="challengeName"
+                value={challengeData.challengeName}
+                onChange={handleInputChange}
                 placeholder="챌린지 제목을 입력해주세요"
                 className="w-full h-full px-[5px] pt-[5px] font-pretendard font-thin text-[20px] leading-[24px] placeholder-[#DFDFDF] text-black focus:outline-none bg-transparent"
               />
@@ -49,6 +82,9 @@ export default function ChallengeMake() {
             {/* 입력창 */}
             <div className="box-border w-full h-[168px] bg-white border border-[#DFDFDF] shadow-[0px_0.5px_2px_rgba(0,0,0,0.25)] rounded-[6px] order-1 flex-none">
               <textarea
+                name="description"
+                value={challengeData.description}
+                onChange={handleInputChange}
                 placeholder="소개글을 입력해주세요"
                 className="w-full h-[168px] resize-none px-[5px] pt-[5px] font-pretendard font-thin text-[20px] leading-[24px] placeholder-[#DFDFDF] text-black focus:outline-none bg-transparent"
               />
@@ -70,7 +106,10 @@ export default function ChallengeMake() {
             {/* 입력창 */}
             <div className="box-border w-full h-[38px] bg-white border border-[#DFDFDF] shadow-[0px_0.5px_2px_rgba(0,0,0,0.25)] rounded-[6px] order-1 flex-none">
               <input
-                type="text"
+                type="number"
+                name="limitAmount"
+                value={challengeData.limitAmount}
+                onChange={handleInputChange}
                 placeholder="최소 1,000원부터 입력"
                 className="w-full h-full px-[5px] pt-[5px] font-pretendard font-thin text-[20px] leading-[24px] placeholder-[#DFDFDF] text-black focus:outline-none bg-transparent"
               />
@@ -80,11 +119,12 @@ export default function ChallengeMake() {
 
         <div className="flex flex-col items-center p-[12px_20px_12px] gap-[22px] relative w-full h-auto bg-white rounded-[6px]">
           <CustomCalendar />
+          {/* 캘린더 선택이벤트 추가 후 날짜 뽑아내기*/}
         </div>
 
         {/* 결제 버튼 */}
         <div className="w-full justify-center flex flex-row">
-          <button className="longButton" onClick={handleClick}>
+          <button className="longButton" onClick={handleSubmit}>
             챌린지 생성
           </button>
         </div>
