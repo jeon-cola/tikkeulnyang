fa48292c52376830768b6666afd009b7d64f473c
이명성 <goodthinglee@gmail.com>
2025-03-31 16:56:34 +0900

    Refactor: 추천카드 매핑된 카테고리 추가

--- BE/src/main/java/com/c107/recommendcard/repository/BudgetCreditMappingRepository.java
+++ BE/src/main/java/com/c107/recommendcard/repository/BudgetCreditMappingRepository.java
@@ -4,7 +4,10 @@ import com.c107.recommendcard.entity.BudgetCreditMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BudgetCreditMappingRepository extends JpaRepository<BudgetCreditMapping, Long> {
     List<BudgetCreditMapping> findByBudgetCategoryId(Integer budgetCategoryId);
+
+    Optional<BudgetCreditMapping> findByCreditBenefitsId(Integer creditBenefitsId);
 }
