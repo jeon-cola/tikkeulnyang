f2e8ed3629fc0f26f0a3609bc8118006951dd130
hongseok <hghg990124@naver.com>
2025-03-31 22:18:52 +0900

    Feat: 카드 디테일 페이지 완성

--- FE/src/features/card/components/DetailCard.jsx
+++ FE/src/features/card/components/DetailCard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom"
 import Api from "../../../services/Api";
 import CustomBackHeader from "../../../components/CustomBackHeader";
+import getIcon from "../assets/getIcon";
 
 export default function DetailCard() {
   const location = useLocation();
@@ -32,14 +33,14 @@ export default function DetailCard() {
       }
     }
     fetchData()
-  },[])
+  },[cardData])
 
   return(
-    <div className="w-full flex flex-col gap-5">
+    <div className="w-full flex flex-col gap-5 mt-10 mb-2">
 
       <CustomBackHeader title={cardList.cardName}/>
 
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center mt-5">
         <img src={`/${cardList.imagePath}`} alt="카드 이미지" style={{width:"225px", height:"360px"}} />
       </div>
 
@@ -47,12 +48,28 @@ export default function DetailCard() {
 
       <p className="text-xl">{cardList.corpName}</p>
 
-      <div className="w-full flex flex-col gap-3">
-        <div className="w-full flex flex-col justify-center">
-         {(Array.isArray(cardList.benefits) && cardList.benefits.length > 0)?cardList.benefits.map((benefit)=>
-         
-          <p className="text-left w-1/3">{benefit.category}</p> 
-        ):""}
+      <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center gap-2">
+          {(Array.isArray(cardList.benefits) && cardList.benefits.length > 0) ? cardList.benefits.map((benefit, index) =>
+            <div className="flex gap-5 justify-center items-center w-full" key={index}>
+              <img src={getIcon(benefit.budgetCategory)} alt={`${benefit.budgetCategory} 아이콘`} style={{height:"30px", width:"30px"}}/>
+              <p className="text-left w-1/3 text-xl font-bold">{benefit.category}</p> 
+            </div>
+          ) : ""}
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col">
+
+          <p className="text-left text-xl font-bold">주요혜택</p>
+
+        <div className="w-full flex flex-col justify-center items-center gap-4">
+          {(Array.isArray(cardList.benefits) && cardList.benefits.length > 0) ? cardList.benefits.map((benefit, index) =>
+            <div className="flex gap-5 justify-center items-center w-full" key={index}>
+              <li className="text-left w-1/3">{benefit.category}</li> 
+              <p className="text-left w-1/2">{benefit.description}</p> 
+            </div>
+          ) : ""}
         </div>
       </div>
       
