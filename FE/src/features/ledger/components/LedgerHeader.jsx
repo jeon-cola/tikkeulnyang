a2f381c33bcaf39b3b69972337c07486d576e92e
Seoyeon Kang <ekfa29@naver.com>
2025-03-31 10:34:18 +0900

    Feat: 공유 가계부 페이지 복구

--- FE/src/features/ledger/components/LedgerHeader.jsx
+++ FE/src/features/ledger/components/LedgerHeader.jsx
@@ -5,6 +5,7 @@ import PurseImg from "../assets/money_purse.png";
 
 export default function LedgerHeader() {
   const navigate = useNavigate();
+  const isSharePage = location.pathname.includes("share");
   const isDetailPage = location.pathname.includes("detail");
   const userInfo = useSelector((state) => state.user);
 
@@ -13,13 +14,29 @@ export default function LedgerHeader() {
     <div className="w-full item-center flex flex-col gap-[10px]">
       {!isDetailPage && (
         <>
-          <Box text={`${userInfo.nickName}님의 가계부`} variant="title">
-            <button
-              className="blackButton "
-              onClick={() => navigate("/ledger/detail")}
-            >
-              세부내역
-            </button>
+          <Box
+            text={
+              isSharePage
+                ? `${userInfo.nickName}님의 공유 가계부`
+                : `${userInfo.nickName}님의 가계부`
+            }
+            variant="title"
+          >
+            {" "}
+            <div className="flex flex-row gap-[5px]">
+              <button
+                className={isSharePage ? "blackButton" : "whiteButton"}
+                onClick={() => navigate("/ledger/share")}
+              >
+                공유
+              </button>
+              <button
+                className={isSharePage ? "whiteButton" : "blackButton"}
+                onClick={() => navigate("/ledger")}
+              >
+                개인
+              </button>
+            </div>
           </Box>
 
           {/* 두번재 박스 : 예산생성 */}
