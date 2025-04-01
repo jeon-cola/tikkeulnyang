5fc869eca10f69d5966257aa6133f275450b3024
dydkssk147258 <dydkssk147258@naver.com>
2025-04-01 11:03:04 +0900

    Fix: 친구 추가 버튼 재 추가

--- FE/src/features/ledger/SharedLedger.jsx
+++ FE/src/features/ledger/SharedLedger.jsx
@@ -3,10 +3,12 @@ import Container from "@/components/Container";
 import CustomCalendar from "@/components/CustomCalendar";
 import LedgerHeader from "./components/LedgerHeader";
 import AddUser from "./assets/add_user.png";
+import Modal from "@/components/Modal";
 import BlackCat from "./assets/ledger_cat.png";
 import Api from "@/services/Api";
 
 export default function SharedLedger() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [value, setValue] = useState(new Date());
   const [calendarData, setCalendarData] = useState([]);
 
@@ -41,6 +43,12 @@ export default function SharedLedger() {
         <div className="relative">
           <div className="flex">
             <img
+              className="w-[20%] cursor-pointer"
+              src={AddUser}
+              alt="사용자 추가"
+              onClick={() => setIsModalOpen(true)} // 모달 열기
+            />
+            <img
               className="absolute -top-1 right-3 z-10 w-[20%] h-auto"
               src={BlackCat}
               alt="캣 이미지"
@@ -65,6 +73,14 @@ export default function SharedLedger() {
               return null;
             }}
           />
+          {/* 모달 컴포넌트 렌더링 */}
+          {isModalOpen && (
+            <Modal
+              title="사용자 추가"
+              description="함께 가계부를 작성할 사용자를 추가하세요."
+              onClose={() => setIsModalOpen(false)} // 모달 닫기
+            />
+          )}
         </div>
       </Container>
     </div>
