4482d357bb04f320847f226d6bfce06001743928
Seoyeon Kang <ekfa29@naver.com>
2025-04-01 10:18:24 +0900

    Feat: 모달연결

--- FE/src/components/CustomBackHeader.jsx
+++ FE/src/components/CustomBackHeader.jsx
@@ -3,20 +3,25 @@ import { useNavigate } from "react-router-dom";
 export default function CustomBackHeader({ title }) {
   const nav = useNavigate();
 
-  function backHandler(e){
+  function backHandler(e) {
     e.preventDefault();
-    nav(-1)
+    nav(-1);
   }
 
-    return (
-      <header className="w-full h-12 bg-white shadow-md fixed top-0 left-0 right-0 z-50 flex justify-center">
-        <div className="absolute left-4 h-full flex items-center">
-          <img src="/Back.svg" alt="뒤로가기" className="h-6 w-6" onClick={backHandler}/>
-        </div>
-        
-        <div className="h-full flex items-center">
-          <p className="text-[23px] font-bold">{title}</p>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className="w-full h-12 bg-white shadow-md fixed top-0 left-0 right-0 z-50 flex justify-center">
+      <div className="absolute left-4 h-full flex items-center">
+        <img
+          src="/Back.svg"
+          alt="뒤로가기"
+          className="h-6 w-6"
+          onClick={backHandler}
+        />
+      </div>
+
+      <div className="h-full flex items-center">
+        <p className="text-[23px] font-bold">{title}</p>
+      </div>
+    </header>
+  );
+}
--- FE/src/features/ledger/SharedLedger.jsx
+++ FE/src/features/ledger/SharedLedger.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 import Container from "@/components/Container";
 import CustomCalendar from "@/components/CustomCalendar";
 import LedgerHeader from "./components/LedgerHeader";
 import AddUser from "./assets/add_user.png";
 import BlackCat from "./assets/ledger_cat.png";
+import Modal from "@/components/Modal";
 
 export default function SharedLedger() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="w-full">
       <Container>
         <LedgerHeader />
         <div className="relative">
-          {/* flex : 왼쪽부터 차례대로 배치치 */}
+          {/* 사용자 추가 버튼 */}
           <div className="flex">
-            <img className="w-[20%]" src={AddUser} alt="사용자 추가" />
             <img
-              className="absolute -top-1 right-3 z-10 w-[20%] h-auto "
+              className="w-[20%] cursor-pointer"
+              src={AddUser}
+              alt="사용자 추가"
+              onClick={() => setIsModalOpen(true)} // 모달 열기
+            />
+            <img
+              className="absolute -top-1 right-3 z-10 w-[20%] h-auto"
               src={BlackCat}
               alt="캣 이미지"
             />
           </div>
+
           <CustomCalendar className="z-0" />
+
+          {/* 모달 컴포넌트 렌더링 */}
+          {isModalOpen && (
+            <Modal
+              title="사용자 추가"
+              description="함께 가계부를 작성할 사용자를 추가하세요."
+              onClose={() => setIsModalOpen(false)} // 모달 닫기
+            />
+          )}
         </div>
       </Container>
     </div>
