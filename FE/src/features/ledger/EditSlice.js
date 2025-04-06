8673c39b1f59ae6944072fdac6dea2496655371f
Seoyeon Kang <ekfa29@naver.com>
2025-04-07 07:37:56 +0900

    Feat: 삭제 api 연결 보완 필요

--- FE/src/features/ledger/EditSlice.js
+++ FE/src/features/ledger/EditSlice.js
@@ -1,20 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { UNSAFE_SingleFetchRedirectSymbol } from "react-router-dom";
 
+// 편집 모드 상태를 관리하는 리덕스 슬라이스
 const editSlice = createSlice({
-  name: "editMode",
+  name: "edit",
   initialState: {
-    isEditMode: false,
+    editMode: false, // 기본적으로 편집 모드는 꺼져 있음
   },
   reducers: {
+    // 편집 모드 토글 액션
     toggleEditMode: (state) => {
-      state.isEditMode = !state.isEditMode;
+      state.editMode = !state.editMode;
     },
+    // 편집 모드 설정 액션
     setEditMode: (state, action) => {
-      state.isEditMode = action.payloadW;
+      state.editMode = action.payload;
     },
   },
 });
 
+// 액션 생성자 및 리듀서 내보내기
 export const { toggleEditMode, setEditMode } = editSlice.actions;
 export default editSlice.reducer;
