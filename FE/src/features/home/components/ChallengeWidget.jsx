ea94761406b3bc7a66132616557856d16713b315
김성준 <sjkim2243@gmail.com>
2025-04-06 19:47:43 +0900

    Design: 메인페이지 위젯

--- FE/src/features/home/components/ChallengeWidget.jsx
+++ FE/src/features/home/components/ChallengeWidget.jsx
@@ -1,11 +1,24 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function ChallengeWidget({
   challengeId = "1",
   thumbnailUrl = "undefined",
   challengeName = "undefined",
+  endDate = "undefined",
 }) {
   const navigate = useNavigate();
+  const [remainDate, setRemainDate] = useState("");
+
+  useEffect(() => {
+    // prop으로 받은 endDate와 현재 날짜의 차이를 구해, remainDate에 저장
+    const endDateObj = new Date(endDate);
+    const currentDate = new Date();
+    const diffTime = endDateObj.getTime() - currentDate.getTime();
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    setRemainDate(diffDays);
+  }, [endDate]);
+
   return (
     <>
       <div
@@ -20,15 +33,15 @@ export default function ChallengeWidget({
           navigate(`/challenge/${challengeId}`);
         }}
       >
-        <div className="absolute inset-0 bg-black/25 backdrop-blur-sm rounded-[6px]"></div>
+        <div className="absolute inset-0 bg-black/25 backdrop-blur-xs rounded-[6px]"></div>
 
         <div className="z-10">
           <h2 className="pt-[18px] font-semibold text-xl text-left leading-[17px] tracking-[0.01em] text-white mb-[6px]">
             {challengeName} 챌린지
           </h2>
 
-          <div className="font-light text-lg text-right pt-17 leading-7 tracking-[0.01em] text-white">
-            {challengeName} 챌린지
+          <div className="font-light text-lg text-right pt-24 leading-7 tracking-[0.01em] text-white">
+            챌린지 종료까지 {remainDate}일
           </div>
 
           {/* <div className="pt-[99px] w-full justify-center flex flex-row"></div> */}
