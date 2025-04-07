e61a5beeef213b87b6f85b8a0fff1672a99dd823
hongs <hghg990124@naver.com>
2025-04-07 17:56:11 +0900

    Feat: 삭제 버튼 추가

--- FE/src/features/home/HomeMain.jsx
+++ FE/src/features/home/HomeMain.jsx
@@ -3,11 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import Api from "../../services/Api";
 import { setEmail, setNickName, setProfileImg } from "../user/UserSlice";
 import axios from "axios";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import SubScribe from "./components/SubScribe";
 import RenderHome from "@/features/home/RenderHome";
 
 export default function HomeMain() {
+  const nav = useNavigate()
+  const {isAuthenticated} = useSelector((state)=> state.user)
+  useEffect(()=>{
+    if (!isAuthenticated) {
+      nav("/user")
+    }
+  })
+  if (!isAuthenticated) return null
   return (
     <Routes>
       <Route path="/" element={<RenderHome />} />
