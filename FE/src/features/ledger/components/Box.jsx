32a35e088e7e2504b952daf7852264ec1b4e1f3c
Seoyeon Kang <ekfa29@naver.com>
2025-04-08 17:42:47 +0900

    Feat: 가계부 세부내역 로딩 기능 추가

--- FE/src/features/ledger/components/Box.jsx
+++ FE/src/features/ledger/components/Box.jsx
@@ -2,8 +2,8 @@ export default function Box({ text, variant, children, onClick }) {
   const baseStyles =
     "w-full h-[50px] flex items-center justify-between rounded-md shadow-sm px-[10px]";
   const variants = {
-    title: "font-medium text-black bg-white text-[19px]",
-    highlight: "font-medium text-black bg-[#FFF0BA] text-[18px]",
+    title: "font-medium text-black bg-white text-[17px]",
+    highlight: "font-medium text-black bg-[#FFF0BA] text-[17px]",
   };
   // 유효성 검사
   const selectedVariant = variants[variant] ?? variants.title;
